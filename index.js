82d6db2614956db2f30336ddc36345c51d3580e5
Ari Sweedler <ari@sweedler.com>
2019-01-17 15:51:14 -0800

    Added lose feedback. Time to push to heroku?

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
+    width: 1500,
     height: 600,
     backgroundColor: "#000044",
     physics: {
@@ -29,26 +29,27 @@ function create ()
 {
   /* set the bounds of our Matter.js world */
   this.matter.world.setBounds(0, 0, game.config.width, game.config.height);
+  document.querySelector("canvas").style.border = "10px solid green";
+  this.add.text(100, 50, 'Try to keep the pendulum up! Use the left/right arrow keys');
 
   /* make a moveable platform (don't let it rotate) */
-  this.platform = this.matter.add.image(400, config.height, 'platform', null, { ignoreGravity: true });
+  this.platform = this.matter.add.image(400, config.height, 'platform');
   this.platform.setFixedRotation();
-  this.platform.setMass(500);
+  this.platform.setMass(8000);
 
   /* add a pendulum head */
   this.pendulumHead = this.matter.add.image(400, 50, 'ball');
   this.pendulumHead.setCircle();
-  this.pendulumHead.setScale(6); //make it 4 times bigger
-  this.pendulumHead.setMass(100);
+  this.pendulumHead.setScale(6);
+  this.pendulumHead.setMass(8000);
 
-  /* attach a pole to the bottom of the ball, and to the top of the platform */
-  this.pendulumNeck = this.matter.add.image(300, 150, 'bar');
-  this.pendulumNeck.setScale(4, 1); //make it 4 times longer
-  this.pendulumNeck.setMass(10);
 
-  this.matter.add.joint(this.pendulumHead, this.pendulumNeck, 60, 0.5);
-  // this.matter.add.joint(this.platform, this.pendulumNeck, 70, 0.5);
-  //TODO how to add joints to a POINT on an object, not THE object??
+  /* Give the pendulum an initial velocity from 20 to 60 */
+  const startVelocity = Math.floor(Math.random() * 40 + 20);
+  this.platform.setVelocityX(startVelocity);
+
+  // TODO how to add joints to a POINT on an object, not THE object??
+  this.matter.add.joint(this.platform, this.pendulumHead, 400, 1);
 
   /* set up keyboard input */
   this.cursors = this.input.keyboard.createCursorKeys();
@@ -56,9 +57,8 @@ function create ()
 
 function update(time, delta) {
 
-  if (this.cursors.left.isDown) {
-    console.log(`Time: ${time}. Delta: ${delta}`);
-  }
+  /* NOTE: You get a time delta here, but I'm not sure if Phaser uses this to move stuff. */
+  console.log(`Time: ${time}. Delta: ${delta}`);
 
   if (this.cursors.left.isDown){
     this.platform.setVelocityX(-20);
@@ -68,16 +68,10 @@ function update(time, delta) {
     this.platform.setVelocityX(0);
   }
 
-  if (this.cursors.up.isDown){
-    this.platform.setVelocityY(-20);
-  } else if (this.cursors.down.isDown) {
-    this.platform.setVelocityY(20);
-  } else {
-    this.platform.setVelocityY(0);
-  }
-
-  //Collider physics happens at the end
-  if (this.pendulumHead.body.position.y > 500) {
-    console.log("you lose");
+  /* Do collider physics at the end */
+  if (this.pendulumHead.body.position.y > 540 && this.gameOver != true) {
+    this.gameOver = true;
+    document.querySelector("canvas").style.borderColor = "red";
+    document.getElementById("you-lose").style.visibility = "visible";
   }
 }
